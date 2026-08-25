fa0dec3b53b9bf57dd4387567615c0f33fcbbc4d
unknown <you@example.com>
2026-08-25 16:32:46 -0300

    feat: Criacao do frontent. + Criacao da estrutura do modal adicionar carro. + Criacao das funcionalidades no carroView.

---
+++ Front/wwwroot/js/carro/carroView.js
@@ -0,0 +1,6 @@
+﻿const inputMarca = document.getElementById("inputMa");
+const inputModelo = document.getElementById("inputMo");
+const inputAnoF = document.getElementById("inputAF");
+const inputCor = document.getElementById("inputC");
+const inputPreco = document.getElementById("inputP");
+
