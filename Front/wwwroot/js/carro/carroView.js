212c8039f3cb417ed48ca297f322e365133ad92f
unknown <you@example.com>
2026-09-11 17:56:10 -0300

    Feat: Funcao de upload de imagens do veiculo adicionada + Novo card de exibicao de carros

--- Front/wwwroot/js/carro/carroView.js
+++ Front/wwwroot/js/carro/carroView.js
@@ -6,10 +6,12 @@ let inputPreco = document.getElementById("inputP");
 const selectMarcaCarro = document.getElementById("marcasCarro");
 const selectModeloCarro = document.getElementById("modelosCarro");
 //EDIT CARRO MODAL
+const input = document.getElementById('imagemInput');
+const fileName = document.getElementById('fileName');
 const inputEditAnoF = document.getElementById("inputEditAF");
 const inputEditCor = document.getElementById("inputEditC");
 const inputEditPreco = document.getElementById("inputEditP");
-const editcarro = document.getElementById("editCarro");
+const btnEditCarro = document.getElementById("editCarro");
 //ADD MARCA CARRO MODAL
 let inputMarca = document.getElementById("inputAddMod");
 const btnAddMarca = document.getElementById("btnAddMarca");
@@ -37,6 +39,10 @@ let page = 1;
 let totalPaginas = 1;
 
 try {
+    input.addEventListener('change', function () {
+        fileName.textContent = this.files[0]?.name ?? 'Nenhuma imagem selecionada';
+    });   
+
     renderCarros(page);
     adcarro.addEventListener('click', async () => {
         enviarCarro()
@@ -105,19 +111,23 @@ async function renderCarros() {
     dados.items.forEach((item) => {
 
         cardCarros += `
-            <div class="card mt-5 rounded-3 col-3" style="width: 18rem; background: #FFDEAD;">
-                <div class="card-body rounded-3">
-                    <h3 class="card-title">${item.nomeModelo}</h3>
-                    <h6 class="card-subtitle mb-2 text-body-secondary" >${item.nomeMarca}</h6>
-                    <p class="card-text text-start fw-bold mb-1" >Ano: ${item.ano}</p>
-                    <p class="card-text text-start fw-bold mb-1" >Cor: ${item.cor}</p>
-                    <h3 class="card-title mb-3" >R$ ${item.preco}</h3>
-                    <div class="justify-content-between d-flex">
-                        <button id="btnEditar" data-modelo=${item.modelo} data-marca=${item.marca} data-ano=${item.ano}  data-preco=${item.preco} class="btn editar btn-warning fs-6 fw-bold rounded-3" data-bs-toggle="modal" data-bs-target="#modalEditar">Editar</button>
-                        <button class="btn reservar btn-success fs-6 fw-bold rounded-3" data-bs-toggle="modal" data-bs-target="#modalReserva">Reservar</button>
-                    </div>
+            <div class="col-md-4 card shadow p-4 my-4 col-12">
+                        <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1a091a20952%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1a091a20952%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.9453125%22%20y%3D%22117.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                        <div class="card-body">
+                            <p class="card-text text-start m-1">Marca: ${item.nomeMarca}</p>
+                            <p class="card-text text-start m-1">Modelo: ${item.nomeModelo}</p>
+                            <p class="card-text text-start m-1">Ano: ${item.ano}</p>
+                            <p class="card-text text-start m-1">Cor: ${item.cor}</p>
+                            <p class="card-text text-center m-1 mb-3 fs-3 text-success">R$ ${item.preco}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-success reservar" data-bs-toggle="modal" data-bs-target="#modalReserva">Reservar</button>
+                                    <button id="btnEditar" type="button" class="btn btn-sm btn-outline-secondary editar" data-modelo=${item.modelo} data-marca=${item.marca} data-ano=${item.ano} data-preco=${item.preco} data-bs-toggle="modal" data-bs-target="#modalEditar">Editar</button>
+                                </div>
+                            </div>
+                        </div>
+
                 </div>
-            </div>
         `;
     });
 
@@ -318,4 +328,8 @@ async function renderSelectModelo() {
     // selectMarcaCarro.innerHTML = optionsMarcas;
 
     selectModeloCarro.innerHTML = optionsModelo;
+}
+
+async function editarCarro() {
+
 }
