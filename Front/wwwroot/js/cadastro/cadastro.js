e76e32d2e6e0302b360182bc55018eb8bf3e4838
Eric Lima <eric.personblack@gmail.com>
2026-09-18 00:05:59 -0300

    Fix: resolvido problemas de enviar usuario

---
+++ Front/wwwroot/js/cadastro/cadastro.js
@@ -0,0 +1,46 @@
+﻿//GERAL 
+let inputNome = document.getElementById("inputNome");
+let inputEmail = document.getElementById("inputEmail");
+let inputSenha = document.getElementById("inputSenha");
+let inputConfirmSenha = document.getElementById("inputConfirmSenha");
+const btnCadastro = document.getElementById("btnCadastro");
+
+try {
+    btnCadastro.addEventListener('click', () => {
+        cadastrarUser();
+    });
+} catch (err) {
+    console.log(err);
+}
+
+async function cadastrarUser() {
+    const nome = inputNome.value;
+    const email = inputEmail.value;
+    const senha1 = inputSenha.value;
+    const senha2 = inputConfirmSenha.value;
+
+    if (senha1 != senha2) {
+        return
+    }
+
+    const payload = {
+        username: nome,
+        email: email,
+        senha: senha2
+    }
+
+    const sendRequest = await fetch("https://localhost:7063/api/user", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+    });
+
+    inputNome.value = "";
+    inputEmail.value = "";
+    inputConfirmSenha.value = "";
+    inputSenha.value = "";
+
+    if (!sendRequest.ok) {
+        console.log("Erro")
+    }
+}
